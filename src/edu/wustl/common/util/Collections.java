5a79691598c811eb4ace3c29ed95e567ad6aca77
Srinath Krishnamurthy <srinath_k>
2008-07-23 13:22:43 +0000

    methods to create Coll from Iterable
    SVN-Revision: 162

--- src/edu/wustl/common/util/Collections.java
+++ src/edu/wustl/common/util/Collections.java
@@ -1,7 +1,12 @@
 package edu.wustl.common.util;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 public class Collections {
     private Collections() {
@@ -54,5 +59,23 @@ public class Collections {
 
     public static <E> Iterator<E> removalForbiddenIterator(Iterable<E> coll) {
         return new RemovalForbiddenIterator<E>(coll.iterator());
+    }
+
+    public static <E> List<E> list(Iterable<E> src) {
+        List<E> res = new ArrayList<E>();
+        fill(src, res);
+        return res;
+    }
+
+    public static <E> Set<E> set(Iterable<E> src) {
+        Set<E> res = new HashSet<E>();
+        fill(src, res);
+        return res;
+    }
+
+    public static <E> void fill(Iterable<E> src, Collection<E> target) {
+        for (E e : src) {
+            target.add(e);
+        }
     }
 }
