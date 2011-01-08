679aa67d6d06c2bdbc283dc676ebf958931a306f
Atul Kaushal <akaushal>
2011-01-08 08:12:08 +0000

    Changes made to remove setAllValues Method.
    SVN-Revision: 325

--- src/java/edu/wustl/common/actionForm/IValueObject.java
+++ src/java/edu/wustl/common/actionForm/IValueObject.java
@@ -2,13 +2,20 @@
 package edu.wustl.common.actionForm;
 
 import edu.wustl.common.domain.AbstractDomainObject;
+import edu.wustl.common.domain.UIRepOfDomain;
 
 /**
  * This provides the interface / API to set values of domain object to form object.
  * @author sachin_lale
  *
+ *
+ * Modified on 07 Jan,2011
+ * Extended IValueObject interface from UIRepOfDomain marker interface.
+ * @author atul_kaushal
+ *
  */
-public interface IValueObject
+//#1
+public interface IValueObject extends UIRepOfDomain
 {
 
 	/**
