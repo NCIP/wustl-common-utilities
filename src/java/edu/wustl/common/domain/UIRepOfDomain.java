c4d79bdd4446ae72ab8fba3ebcbe512d42b1b56e
Syed Rizvi <rizvis>
2013-01-28 10:32:21 +0000

    Initial check-in for the merged WashU-Commons, which will include CommonPackage, DAO, SecurityManager and (old) WashU-Commons. This check-in is a replica of tags/1.3.rc
    SVN-Revision: 350

---
+++ src/java/edu/wustl/common/domain/UIRepOfDomain.java
@@ -0,0 +1,12 @@
+package edu.wustl.common.domain;
+
+/**
+ * Marker interface for any classes that are a UI representation of a domain
+ * object.
+ * 
+ * @author srinath_k
+ * 
+ */
+public interface UIRepOfDomain {
+
+}
