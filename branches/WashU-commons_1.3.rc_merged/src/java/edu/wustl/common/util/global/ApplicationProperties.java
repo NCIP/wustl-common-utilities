0c3e04f4dd67b58a26bed57a50810c1735ddf868
Syed Rizvi <rizvis>
2013-01-28 10:32:21 +0000

    Initial check-in for the merged WashU-Commons, which will include CommonPackage, DAO, SecurityManager and (old) WashU-Commons. This check-in is a replica of tags/1.3.rc
    SVN-Revision: 350

---
+++ branches/WashU-commons_1.3.rc_merged/src/java/edu/wustl/common/util/global/ApplicationProperties.java
@@ -0,0 +1,106 @@
+/*
+ * Created on Jul 19, 2004
+ *
+ */
+
+package edu.wustl.common.util.global;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import edu.wustl.common.util.logger.Logger;
+
+/**
+ * This class is used to retrieve values of keys from the ApplicationResources.properties file.
+ * @author kapil_kaveeshwar
+ */
+public final class ApplicationProperties
+{
+	/**
+	 * private constructor.
+	 */
+	private ApplicationProperties()
+	{
+
+	}
+	/**
+	 * Generic resource bundle.
+	 */
+	private static ResourceBundle bundle;
+
+	/**
+	 * LOGGER Logger - Generic LOGGER.
+	 */
+	private static final Logger LOGGER = Logger.getCommonLogger(ApplicationProperties.class);
+
+	/**
+	 * Load the resource bundle file.
+	 * @param baseName Resource Bundle file name.
+	 */
+	public static void initBundle(String baseName)
+	{
+		bundle = ResourceBundle.getBundle(baseName);
+	}
+
+	/**
+	 * @param theKey key in a application property file
+	 * @return the value of key.
+	 */
+	public static String getValue(String theKey)
+	{
+		String val = CommonConstants.EMPTY_STRING;
+		if (bundle == null)
+		{
+			LOGGER.fatal("resource bundle is null cannot return value for key " + theKey);
+		}
+		else
+		{
+			val = bundle.getString(theKey);
+		}
+		return val;
+	}
+
+	/**
+	 * This method should be used when you want to
+	 * customize error message with multiple replacement parameters.
+	 *
+	 * @param theKey - error key
+	 * @param placeHolders - replacement Strings
+	 * @return - complete error message
+	 */
+	public static String getValue(String theKey, List<String> placeHolders)
+	{
+		String msg = CommonConstants.EMPTY_STRING;
+		if (bundle == null)
+		{
+			LOGGER.fatal("resource bundle is null cannot return value for key " + theKey);
+		}
+		else
+		{
+			msg = bundle.getString(theKey);
+			StringBuffer message = new StringBuffer(msg);
+			for (int i = 0; i < placeHolders.size(); i++)
+			{
+				message.replace(message.indexOf("{"),
+						message.indexOf("}") + 1,placeHolders.get(i));
+			}
+			msg=message.toString();
+		}
+		return msg;
+	}
+
+	/**
+	 * This method should be used when you want to customize error message with single replacement parameter.
+	 * @param theKey - error key
+	 * @param placeHolder - replacement Strings
+	 * @return - complete error message
+	 */
+	public static String getValue(String theKey, String placeHolder)
+	{
+		List<String> temp = new ArrayList<String>();
+		temp.add(placeHolder);
+		return getValue(theKey, temp);
+	}
+
+}
